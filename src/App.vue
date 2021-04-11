11ca64e76fa7a1aee1c0e6adbc0abd6a5a187564
lizardon <chia.qien.don@dhs.sg>
2021-04-11 17:47:49 +0800

    icons updated

--- src/App.vue
+++ src/App.vue
@@ -32,8 +32,6 @@ export default {
 body {
   margin: 0;
   padding: 0;
-  overflow: hidden;
-  display: flex;
 
 }
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,20 +4,23 @@
             <div class="icon"></div>
             <div class="icon"></div>
             <div class="icon"></div>
+            <div class="icon"></div>
+            <div class="icon"></div>
+            <div class="icon"></div>
         </div>
     </div>
 </template>
 
 <style scoped>
 .bar-container {
-    width: 100%;
-    height: 40px;
+    width: auto;
+    height: 70px;
     background-color: rgba(255, 255, 255, .15);  
-    backdrop-filter: blur(5px);  
-    border-radius: 10px;
+    backdrop-filter: blur(10px);  
+    border-radius: 20px;
     border: 1px solid  rgb(255, 255, 255, 0.2);
     display: flex;
-    justify-content: flex-start;
+    justify-content: space-evenly;
     align-items: center;
 }
 
@@ -25,14 +28,22 @@
     padding-right: 20px;
     padding-left: 20px;
     padding-bottom: 10px;
+    display: flex;
+    justify-content: center;
 }
 
 .icon {
-    width: 25px;
-    height: 25px;
-    border-radius: 12px;
-    background: black;
+    width: 45px;
+    height: 45px;
+    border-radius: 10px;
+    background: white;
     justify-content: center;
     align-items: center;
+    margin-left: 10px;
+    margin-right: 10px;
+}
+
+.icon:hover{
+    cursor: pointer;
 }
 </style>
