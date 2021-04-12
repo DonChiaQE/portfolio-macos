df2548d0a01353bc93bda73b8efe8b09acc81f4d
lizardon <chia.qien.don@dhs.sg>
2021-04-12 16:03:44 +0800

    sizing and dragging

--- src/components/NossaflexModal.vue
+++ src/components/NossaflexModal.vue
@@ -1,8 +1,8 @@
 /* eslint-disable */
 <template>
     <div class="flex-container" id="flex-container">
-        <div class="about-me" id="container" :class="{ fullscreen: $store.getters.isFullscreenNossaflex, close: !$store.getters.isShownNossaflex}">
-            <div class="top-bar" id="top-bar" v-on:dblclick="$store.commit('toggleFullscreenNossaflex')">
+        <div class="about-me" id="container" ref="draggableContainer" :class="{ fullscreen: $store.getters.isFullscreenNossaflex, close: !$store.getters.isShownNossaflex}">
+            <div class="top-bar" id="top-bar" v-on:dblclick="$store.commit('toggleFullscreenNossaflex')" @mousedown="dragMouseDown">
                 <div class="triple-button">
                     <div class="button-red" v-on:click="$store.commit('toggleShownNossaflex', false)"></div>
                     <div class="button-yellow"></div>
@@ -11,22 +11,38 @@
             </div>
             <div class="bar"></div>
             <div class="content">
-                <!-- <textarea class="textarea-content">
-
-                </textarea> -->
                 <div class="scroll-container">
-                    <div class="header">NOSSAFLEX</div>
-                    <div class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum lacinia turpis, eu ullamcorper ligula eleifend sed. Cras at diam eros. Phasellus dui nulla, consequat vestibulum nisl nec, efficitur sodales dui. Vivamus sit amet tellus ac enim imperdiet fringilla. Vivamus feugiat fermentum elit in laoreet. Nulla cursus arcu et fermentum efficitur. Sed euismod ultrices scelerisque. Sed malesuada eget nisl nec venenatis. Proin congue vehicula nulla, et auctor nisi. Duis eget leo massa. Nunc at gravida nibh.</div>
-                    <div class="paragraph">Mauris a consectetur lorem. Vestibulum arcu est, imperdiet a sodales eu, feugiat nec lacus. Quisque odio metus, consequat eu neque eu, volutpat finibus elit. Vestibulum pellentesque porta sapien, et accumsan ex ultrices sit amet. Nunc et lorem mollis, dictum libero ac, sollicitudin arcu. Morbi porta lacus eu feugiat tempus. Donec ut libero quam. Curabitur in nibh metus. Aliquam in nisi est. Etiam eget feugiat mauris, non sodales purus. Suspendisse pharetra scelerisque sem, vitae fermentum magna mattis vitae. Curabitur mattis libero vel rutrum dictum.</div>
-                    <div class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum lacinia turpis, eu ullamcorper ligula eleifend sed. Cras at diam eros. Phasellus dui nulla, consequat vestibulum nisl nec, efficitur sodales dui. Vivamus sit amet tellus ac enim imperdiet fringilla. Vivamus feugiat fermentum elit in laoreet. Nulla cursus arcu et fermentum efficitur. Sed euismod ultrices scelerisque. Sed malesuada eget nisl nec venenatis. Proin congue vehicula nulla, et auctor nisi. Duis eget leo massa. Nunc at gravida nibh.</div>
-                    <div class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum lacinia turpis, eu ullamcorper ligula eleifend sed. Cras at diam eros. Phasellus dui nulla, consequat vestibulum nisl nec, efficitur sodales dui. Vivamus sit amet tellus ac enim imperdiet fringilla. Vivamus feugiat fermentum elit in laoreet. Nulla cursus arcu et fermentum efficitur. Sed euismod ultrices scelerisque. Sed malesuada eget nisl nec venenatis. Proin congue vehicula nulla, et auctor nisi. Duis eget leo massa. Nunc at gravida nibh.</div>
+                    <div class="header">lorem</div>
+                    <div class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum lacinia turpis, eu ullamcorper ligula eleifend sed. Cras at diam eros. Phasellus dui nulla, consequat vestibulum nisl nec, efficitur sodales dui. Vivamus sit amet
+                        tellus ac enim imperdiet fringilla. Vivamus feugiat fermentum elit in laoreet. Nulla cursus arcu et fermentum efficitur. Sed euismod ultrices scelerisque. Sed malesuada eget nisl nec venenatis. Proin congue vehicula nulla, et auctor
+                        nisi. Duis eget leo massa. Nunc at gravida nibh.</div>
+                    <div class="paragraph">Mauris a consectetur lorem. Vestibulum arcu est, imperdiet a sodales eu, feugiat nec lacus. Quisque odio metus, consequat eu neque eu, volutpat finibus elit. Vestibulum pellentesque porta sapien, et accumsan ex ultrices sit amet. Nunc
+                        et lorem mollis, dictum libero ac, sollicitudin arcu. Morbi porta lacus eu feugiat tempus. Donec ut libero quam. Curabitur in nibh metus. Aliquam in nisi est. Etiam eget feugiat mauris, non sodales purus. Suspendisse pharetra scelerisque
+                        sem, vitae fermentum magna mattis vitae. Curabitur mattis libero vel rutrum dictum.</div>
+                    <div class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum lacinia turpis, eu ullamcorper ligula eleifend sed. Cras at diam eros. Phasellus dui nulla, consequat vestibulum nisl nec, efficitur sodales dui. Vivamus sit amet
+                        tellus ac enim imperdiet fringilla. Vivamus feugiat fermentum elit in laoreet. Nulla cursus arcu et fermentum efficitur. Sed euismod ultrices scelerisque. Sed malesuada eget nisl nec venenatis. Proin congue vehicula nulla, et auctor
+                        nisi. Duis eget leo massa. Nunc at gravida nibh.</div>
+                    <div class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum lacinia turpis, eu ullamcorper ligula eleifend sed. Cras at diam eros. Phasellus dui nulla, consequat vestibulum nisl nec, efficitur sodales dui. Vivamus sit amet
+                        tellus ac enim imperdiet fringilla. Vivamus feugiat fermentum elit in laoreet. Nulla cursus arcu et fermentum efficitur. Sed euismod ultrices scelerisque. Sed malesuada eget nisl nec venenatis. Proin congue vehicula nulla, et auctor
+                        nisi. Duis eget leo massa. Nunc at gravida nibh.</div>
                 </div>
             </div>
+            <div class="resizer resizer-b"></div>
+            <div class="resizer resizer-l"></div>
+            <div class="resizer resizer-t"></div>
+            <div class="resizer resizer-r"></div>
         </div>
     </div>
 </template>
 
 <style scoped>
+.resize-drag {
+    background: #41b883;
+    min-height: 40px;
+    min-width: 600px;
+    z-index: 10;
+}
+
 .flex-container {
     display: flex;
     justify-content: center;
@@ -37,6 +53,8 @@
 }
 
 .about-me {
+    min-height: 300px;
+    min-width: 600px;
     height: 300px;
     width: 600px;
     border-radius: 15px;
@@ -44,10 +62,24 @@
     overflow: hidden;
     border: 1px solid #dadada;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.15), 0 6px 20px 0 rgba(0, 0, 0, 0.14);
-    transition: all 0.5s ease;
-    margin: 0;
-    margin-left: 10px;
-    margin-right: 10px;
+    /* transition: all 0.5s ease; */
+    
+    max-height: 100%;
+    max-width: 100%;
+    align-items: flex-end;
+    z-index: 9;
+    position: absolute;
+    
+}
+
+.fullscreen {
+    width: 100% !important;
+    height: 100vh !important;
+    margin-left: 0;
+    margin-right: 0;
+    position: fixed  !important; /* or absolute */
+  top: 30px !important;
+  left: 0 !important;
 }
 
 .close {
@@ -63,6 +95,7 @@
     height: 40px;
     width: 100%;
     background: #ECECED;
+    z-index: 10;
 }
 
 .triple-button {
@@ -180,11 +213,40 @@ img {
     padding-bottom: 80px;
 }
 
-.fullscreen {
-    width: 100%;
+.resizer-r {
+    cursor: col-resize;
     height: 100%;
-    margin-left: 0;
-    margin-right: 0;
+    right: 0;
+    top: 0;
+    width: 5px;
+}
+
+.resizer-b {
+    bottom: 0;
+    cursor: row-resize;
+    height: 5px;
+    left: 0;
+    width: 100%;
+}
+
+.resizer-t {
+    top: 0;
+    cursor: row-resize;
+    height: 5px;
+    left: 0;
+    width: 100%;
+}
+
+.resizer-l {
+    cursor: col-resize;
+    height: 100%;
+    left: 0;
+    bottom: 0;
+    width: 5px;
+}
+
+.resizer {
+    position: absolute;
 }
 
 @media (prefers-color-scheme: dark) {
@@ -221,14 +283,169 @@ export default {
     },
     data: function() {
         return {
-
+            positions: {
+                clientX: undefined,
+                clientY: undefined,
+                movementX: 0,
+                movementY: 0
+            }
         }
     },
+    computed: {
+
+    },
     methods: {
-        
+        dragMouseDown: function(event) {
+            event.preventDefault()
+            // get the mouse cursor position at startup:
+            this.positions.clientX = event.clientX
+            this.positions.clientY = event.clientY
+            document.onmousemove = this.elementDrag
+            document.onmouseup = this.closeDragElement
+        },
+        elementDrag: function(event) {
+            event.preventDefault()
+            this.positions.movementX = this.positions.clientX - event.clientX
+            this.positions.movementY = this.positions.clientY - event.clientY
+            this.positions.clientX = event.clientX
+            this.positions.clientY = event.clientY
+            // set the element's new position:
+            this.$refs.draggableContainer.style.top = (this.$refs.draggableContainer.offsetTop - this.positions.movementY) + 'px'
+            this.$refs.draggableContainer.style.left = (this.$refs.draggableContainer.offsetLeft - this.positions.movementX) + 'px'
+        },
+        closeDragElement() {
+            document.onmouseup = null
+            document.onmousemove = null
+        }
     },
     mounted: function() {
-        
+        // Query the element
+        const ele = document.getElementById('container');
+
+        // The current position of mouse
+        let x = 0;
+        let y = 0;
+
+        // The dimension of the element
+        let w = 0;
+        let h = 0;
+
+        const mouseDownHandlerRight = function(e) {
+            x = e.clientX;
+
+            const styles = window.getComputedStyle(ele)
+            w = parseInt(styles.width, 10);
+
+            document.addEventListener('mousemove', mouseMoveHandlerRight);
+            document.addEventListener('mouseup', mouseUpHandlerRight);
+        }
+
+        const mouseDownHandlerLeft = function(e) {
+            x = e.clientX;
+
+            const styles = window.getComputedStyle(ele)
+            w = parseInt(styles.width, 10);
+
+            document.addEventListener('mousemove', mouseMoveHandlerLeft);
+            document.addEventListener('mouseup', mouseUpHandlerLeft);
+        }
+
+        const mouseDownHandlerUp = function(e) {
+            y = e.clientY;
+
+            const styles = window.getComputedStyle(ele)
+            h = parseInt(styles.height, 10);
+
+            document.addEventListener('mousemove', mouseMoveHandlerUp);
+            document.addEventListener('mouseup', mouseUpHandlerUp);
+        }
+
+        const mouseDownHandlerDown = function(e) {
+            y = e.clientY;
+
+            const styles = window.getComputedStyle(ele)
+            h = parseInt(styles.height, 10);
+
+            document.addEventListener('mousemove', mouseMoveHandlerDown);
+            document.addEventListener('mouseup', mouseUpHandlerDown);
+        }
+
+        const mouseMoveHandlerUp = function(e) {
+            // How far the mouse has been moved
+            const dy = e.clientY - y;
+
+            // Adjust the dimension of element
+            ele.style.height = `${h - dy}px`;
+        };
+
+        const mouseMoveHandlerDown = function(e) {
+            // How far the mouse has been moved
+            const dy = e.clientY - y;
+
+            // Adjust the dimension of element
+            ele.style.height = `${h + dy}px`;
+        };
+
+        const mouseMoveHandlerRight = function(e) {
+            // How far the mouse has been moved
+            const dx = e.clientX - x;
+
+            // Adjust the dimension of element
+            ele.style.width = `${w + dx}px`;
+        };
+
+        const mouseMoveHandlerLeft = function(e) {
+            // How far the mouse has been moved
+            const dx = e.clientX - x;
+
+            // Adjust the dimension of element
+            ele.style.width = `${w - dx}px`;
+        };
+
+        const mouseUpHandlerUp = function() {
+            // Remove the handlers of `mousemove` and `mouseup`
+            document.removeEventListener('mousemove', mouseMoveHandlerUp);
+            document.removeEventListener('mouseup', mouseUpHandlerUp);
+        };
+
+        const mouseUpHandlerDown = function() {
+            // Remove the handlers of `mousemove` and `mouseup`
+            document.removeEventListener('mousemove', mouseMoveHandlerDown);
+            document.removeEventListener('mouseup', mouseUpHandlerDown);
+        };
+
+        const mouseUpHandlerRight = function() {
+            // Remove the handlers of `mousemove` and `mouseup`
+            document.removeEventListener('mousemove', mouseMoveHandlerRight);
+            document.removeEventListener('mouseup', mouseUpHandlerRight);
+        };
+
+        const mouseUpHandlerLeft = function() {
+            // Remove the handlers of `mousemove` and `mouseup`
+            document.removeEventListener('mousemove', mouseMoveHandlerLeft);
+            document.removeEventListener('mouseup', mouseUpHandlerLeft);
+        };
+        // Query all resizers
+        const resizersRight = ele.querySelectorAll('.resizer-r');
+        const resizersUp = ele.querySelectorAll('.resizer-t');
+        const resizersDown = ele.querySelectorAll('.resizer-b');
+        const resizersLeft = ele.querySelectorAll('.resizer-l');
+
+        [].forEach.call(resizersRight, function(resizer) {
+            resizer.addEventListener('mousedown', mouseDownHandlerRight);
+        });
+
+        [].forEach.call(resizersUp, function(resizer) {
+            resizer.addEventListener('mousedown', mouseDownHandlerUp);
+        });
+
+        [].forEach.call(resizersDown, function(resizer) {
+            resizer.addEventListener('mousedown', mouseDownHandlerDown);
+        });
+
+        [].forEach.call(resizersLeft, function(resizer) {
+            resizer.addEventListener('mousedown', mouseDownHandlerLeft);
+        });
     }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import { store } from './store/store'
 import moment from 'moment'
+import VueInteractJs from "vue-interactjs";
+
+Vue.use(VueInteractJs);
 
 Vue.prototype.moment = moment
 Vue.config.productionTip = false
